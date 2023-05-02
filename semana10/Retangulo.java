ff6d20cb4a5af6f7501d79fcbb034bec6b3aca61
csoodre <126599223+csoodre@users.noreply.github.com>
2023-05-02 15:28:06 -0300

    Add files via upload

---
+++ semana10/Retangulo.java
@@ -0,0 +1,13 @@
+public class Retangulo{
+    //Atributos
+    public double base;
+    public double altura;
+
+    //Métodos
+    public double calcularArea() {
+        return base * altura;
+    }
+    public double calcularPerimetro() {
+        return 2 * (base + altura);
+    }
+}
